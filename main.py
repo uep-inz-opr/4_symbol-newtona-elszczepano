31c6c9de27d72164d5ab50a7c7dfeed3638c2fb4
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2022-04-19 12:11:56 +0200

    Print int

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ n = int(elements[0])
 k = int(elements[1])
 
 def newton(n,k):
-    print(math.factorial(n)/(math.factorial(k)*math.factorial(n-k)))
+    print(int(math.factorial(n)/(math.factorial(k)*math.factorial(n-k))))
 
 newton(n,k)
