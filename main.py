471f5e9c55e41ebf1e3b429c0dc53ce3121a1e86
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2022-04-19 12:10:08 +0200

    Fix input

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import math
 
-n = int(input())
-k = int(input())
+elements = input().split( " " )
+
+n = int(elements[0])
+k = int(elements[1])
 
 def newton(n,k):
     print(math.factorial(n)/(math.factorial(k)*math.factorial(n-k)))
